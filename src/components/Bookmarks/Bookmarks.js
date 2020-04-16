50f1639b6a95804ee531176e383efff7025e4ecc
monstaro <codysmith2814@gmail.com>
2020-04-16 16:44:42 -0600

    Adds testid

--- src/components/Bookmarks/Bookmarks.js
+++ src/components/Bookmarks/Bookmarks.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import './Bookmarks.scss'
+import React from "react";
+import "./Bookmarks.scss";
 
 const Bookmarks = () => {
-    return (
-        <div className="bookmarks-container">
-            <h2 className="bookmarks-header">
-                Bookmarks
-            </h2>
-        </div>
-    )
-}
+  return (
+    <div className="bookmarks-container" data-testid="bookmarks-container">
+      <h2 className="bookmarks-header">Bookmarks</h2>
+    </div>
+  );
+};
 
 export default Bookmarks;
