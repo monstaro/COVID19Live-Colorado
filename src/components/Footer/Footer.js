d28b0a0be081a286850898c5d71c8a9f15a7babb
monstaro <codysmith2814@gmail.com>
2020-04-15 20:04:28 -0600

    Adds footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <span className="copyright-symbol">
+               &copy;
+            </span>
+             <span className="copyright">
+               2020 <a alt="link to github" 
+                       className="github-link"
+                       href="https://github.com/monstaro"
+                       rel="noopener noreferrer"
+                       target="_blank">
+                           Cody Smith
+                    </a>
+             </span>
+        </div>
+    )
+}
+
+export default Footer;
