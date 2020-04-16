6c2c60065f043156bfad92d31d9d7da810d3edf2
monstaro <codysmith2814@gmail.com>
2020-04-16 14:37:17 -0600

    moves fetch to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Welcome from "../Welcome/Welcome";
@@ -11,53 +11,65 @@ import StayingProtected from "../StayingProtected/StayingProtected";
 import Bookmarks from "../Bookmarks/Bookmarks";
 import LocalHealth from "../LocalHealth/LocalHealth";
 import CountyStats from "../CountyStats/CountyStats";
-import { fetchCounties } from "../../apiCalls"
+import { loadCounties } from "../../actions"
+import { fetchCounties } from "../../apiCalls";
+import { connect } from "react-redux";
 
-function App() {
-  fetchCounties()
-  return (
-    <div className="App">
-      <Header className="header" />
-      <Nav className="nav" />
-      <Sidebar className="sidebar" />
-      <Switch>
+class App extends Component {
+  componentDidMount() {
+    fetchCounties()
+    .then(data => this.props.loadCounties(data))
+    .catch(err => console.error(err.message))
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header className="header" />
+        <Nav className="nav" />
+        <Sidebar className="sidebar" />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={() => <Welcome className="main-area" />}
+          />
+        </Switch>
         <Route
-          path="/"
+          path="/about"
           exact
-          component={() => <Welcome className="main-area" />}
+          component={() => <About className="main-area" />}
         />
-      </Switch>
-      <Route
-        path="/about"
-        exact
-        component={() => <About className="main-area" />}
-      />
-      <Route
-        path="/staying-protected"
-        exact
-        component={() => <StayingProtected className="main-area" />}
-      />
-      <Route
-        path="/county-stats"
-        exact
-        component={() => <CountyStats className="main-area" />}
-      />
-      <Route
-        path="/bookmarks"
-        exact
-        component={() => <Bookmarks className="main-area" />}
-      />
-      <Route 
-        path="/local-health"
-        exact
-        component={() => <LocalHealth />} />
-      <Route 
-        path="/welcome"
-        exact
-        component={() => <Welcome />} />
-      <Footer className="footer" />
-    </div>
-  );
+        <Route
+          path="/staying-protected"
+          exact
+          component={() => <StayingProtected className="main-area" />}
+        />
+        <Route
+          path="/county-stats"
+          exact
+          component={() => <CountyStats className="main-area" />}
+        />
+        <Route
+          path="/bookmarks"
+          exact
+          component={() => <Bookmarks className="main-area" />}
+        />
+        <Route 
+          path="/local-health"
+          exact
+          component={() => <LocalHealth />} />
+        <Route 
+          path="/welcome"
+          exact
+          component={() => <Welcome />} />
+        <Footer className="footer" />
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  loadCounties: counties => dispatch(loadCounties(counties))
+})
+
+export default connect(null, mapDispatchToProps)(App)
--- src/components/LocalHealth/LocalHealth.js
+++ src/components/LocalHealth/LocalHealth.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './LocalHealth.scss'
 
-const LocalHealth = () => {
-    return (
-        <div className="local-health-container">
-            <h2 className="local-health-header">
-                Local Health Depts
-            </h2>
-        </div>
-    )
+class LocalHealth extends Component {
+    render() {
+        return (
+            <div className="local-health-container">
+                <h2 className="local-health-header">
+                    Local Health Depts
+                </h2>
+                <h3 className="">
+                    Select your county below:
+                </h3>
+            </div>
+        )
+    }
 }
 
 export default LocalHealth;
