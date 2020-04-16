bc3c7fac53ee5b50732867c09972c72add63f2c3
monstaro <codysmith2814@gmail.com>
2020-04-15 19:30:16 -0600

    Creates initial layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Welcome from '../Welcome/Welcome'
+import Header from '../Header/Header'
+import Sidebar from '../Sidebar/Sidebar'
+import Nav from '../Nav/Nav'
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Sidebar />
-      <Nav />
+      <Header className="header"/>
+      <Nav className="nav"/>
+      <Sidebar className="sidebar"/>
       <Switch>
         <Route
           path="/" exact
