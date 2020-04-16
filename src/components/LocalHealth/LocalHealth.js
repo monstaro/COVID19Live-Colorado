51b6fe0bc03263049f55c6521524bd39e2583b82
monstaro <codysmith2814@gmail.com>
2020-04-16 15:43:08 -0600

    Fixes targeting of element

--- src/components/LocalHealth/LocalHealth.js
+++ src/components/LocalHealth/LocalHealth.js
@@ -1,30 +1,39 @@
-import React, { Component } from 'react'
-import './LocalHealth.scss'
-import { fetchHealthDepts } from '../../apiCalls'
-import { loadHealthDepts } from '../../actions'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import "./LocalHealth.scss";
+import { fetchHealthDepts } from "../../apiCalls";
+import { loadHealthDepts } from "../../actions";
+import { connect } from "react-redux";
 
 class LocalHealth extends Component {
-    componentDidMount() {
-        fetchHealthDepts()
-        .then(data => this.props.loadHealthDepts(data))
-    }
-    render() {
-        return (
-            <div className="local-health-container">
-                <h2 className="local-health-header">
-                    Local Health Depts
-                </h2>
-                <h3 className="">
-                    Select your county below:
-                </h3>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedCounty: null,
+    };
+  }
+  componentDidMount() {
+    fetchHealthDepts().then((data) => this.props.loadHealthDepts(data));
+  }
+  render() {
+    console.log(this.props);
+
+    return (
+      <div className="local-health-container">
+        <h2 className="local-health-header">Local Health Depts</h2>
+        <h3 className="">Select your county below:</h3>
+      </div>
+    );
+  }
 }
 
-const mapDispatchToProps = dispatch => ({
-    loadHealthDepts: healthDepts => dispatch(loadHealthDepts(healthDepts))
-})
+const mapStateToProps = (state) => {
+  return {
+    depts: state.healthDepts,
+  };
+};
 
-export default connect(null, mapDispatchToProps)(LocalHealth)
+const mapDispatchToProps = (dispatch) => ({
+  loadHealthDepts: (healthDepts) => dispatch(loadHealthDepts(healthDepts)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LocalHealth);
--- src/reducers/healthDepts.js
+++ src/reducers/healthDepts.js
@@ -2,7 +2,7 @@ export const healthDepts = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case "LOAD_HEALTH_DEPTS":
-      return [...state, action.healthDepts];
+      return [...state, ...action.healthDepts];
     default:
       return state;
   }
