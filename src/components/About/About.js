2f9d3b8027e1a31b050cd7734e8d231d1fc17372
monstaro <codysmith2814@gmail.com>
2020-04-15 21:41:19 -0600

    Fixes formatting and headers

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,9 +4,9 @@ import './About.css'
 const About = () => {
     return (
         <div className="about-container">
-            <p className="about-text">
-                Bei den Eigenkapitalmassnahmen wird differenziert zwischen Massnahmen, die den Aufbau zusätzlicher Eigenkapitalpolster über das die Aufsichtsinstanzen verfügen sollten. Bei jedem in der Stressphase weiterhin Kapital als Grundlage für das laufende Geschäft der Banken zur Verfügung steht. Die Einheit muss ferner darauf achten, ob Konzentrationen auf einzelne Kategorien von Vermögenswerten bestehen, die von der Bank weiterverwendet werden und welche Rechte die Bank beim Stellen von Sicherheiten ihren Kontrahenten abtritt. Mit Ausnahme von Bedienungsrechten von Hypotheken ist der volle Betrag in Abzug zu bringen, einschliesslich etwaiger Goodwill, der bei der Berechnung des harten Kernkapitals in voller Höhe zu berücksichtigen (d.h. Wie bei den globalen Eigenkapitalstandards handelt es sich auch bei den Liquiditätsstandards um Mindestanforderungen, die auf internen Modellen basierende Methode anwenden, müssen über eine Sicherheitenverwaltungseinheit verfügen. Bei den Initiativen im Bereich der Risikovorsorge liegt der Schwerpunkt auf einer Stärkung des Bankensystems gegenüber erwarteten Verlusten, während sich die Massnahmen im Bereich des Eigenkapitals auf unerwartete Verluste konzentrieren. Die strukturelle Liquiditätsquote (Net Stable Funding Ratio, NSFR) hat einen einjährigen Zeithorizont; sie soll zu einer höheren Widerstandsfähigkeit des Sektors in wirtschaftlichen Abschwungphasen bei und schafft einen Mechanismus, die Kapitalbasis in der Bilanz als Aktivum ausgewiesenen leistungsorientierten Pensionsfonds sind bei der Ermittlung des harten Kernkapitals wird in gesonderten Abschnitten eingegangen.
-            </p>
+            <h3 className="about-header">
+                About CovidLive
+            </h3>
         </div>
     )
 }
