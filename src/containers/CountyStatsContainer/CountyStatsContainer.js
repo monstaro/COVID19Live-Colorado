b3e93ce7fe1b9152edd72ebb99fa135054c022de
monstaro <codysmith2814@gmail.com>
2020-04-17 17:03:20 -0600

    Changes prop structure

--- src/containers/CountyStatsContainer/CountyStatsContainer.js
+++ src/containers/CountyStatsContainer/CountyStatsContainer.js
@@ -9,7 +9,10 @@ class CountyStats extends Component {
     super(props);
     this.state = {
       selectedCounty: null,
-      selectedCountyDeaths: null
+      selectedCountyDeaths: null,
+      deaths: null,
+      cases: null,
+      countyPop: null
     };
   }
   getCountyNames = () => {
@@ -22,11 +25,22 @@ class CountyStats extends Component {
     return counties
   }
   selectCounty = (county) => {
-
+    console.log(this.props.counties.find(countyObject => countyObject.FULL_ === county))
     this.setState({
-      selectedCounty: this.props.counties.find(countyObject => countyObject.FULL_ === county),
-      selectedCountyDeaths: this.props.countyDeaths.filter(co => co.county === county.split(' ')[0])
+      countyName: this.props.counties.find(countyObject => countyObject.FULL_ === county).FULL_ + ':',
+      deaths: this.returnCurrentCountyInfo(county),
+      cases: this.props.counties.find(countyObject => countyObject.FULL_ === county).County_Pos_Cases,
+      countyPop: this.props.counties.find(countyObject => countyObject.FULL_ === county).County_Population
     })
+  }
+  returnCurrentCountyInfo(county) {
+      let a = this.props.countyDeaths.filter(co => co.county === county.split(' ')[0])
+      let b = a[a.length - 1]
+      if (b) {
+        return parseInt(b.deaths)
+      } else {
+        return 0
+      }
   }
   render() {
     if (this.props.counties.length) {
@@ -39,13 +53,18 @@ class CountyStats extends Component {
           <h3 className="county-stats-subheader">Select A County Below</h3>
           <section className="county-picker"></section>
           <CountyDropdown countyNames={this.getCountyNames()} selectCounty={(county) => this.selectCounty(county)}/>
-          <CountyData countyDeaths={this.state.selectedCountyDeaths}/>
+          {this.state.countyName}
+          <CountyData
+             deaths={this.state.deaths || 0} 
+             cases={this.state.cases || 0}
+             countyPop={this.state.countyPop || 0}
+             />
         </div>
       );
     } else {
       return (
-        <div data-testid="county-stats-container">
-          'Loading...'
+        <div className="county-stats-container" data-testid="county-stats-container">
+          <h2 className="county-stats-header">County Stats</h2>
         </div>
       )
     }
