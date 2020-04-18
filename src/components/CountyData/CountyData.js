0e6ed53dad68e1083b73679b47fc4ce3a52c9e22
monstaro <codysmith2814@gmail.com>
2020-04-18 13:34:44 -0600

    Fixes formatting

--- src/components/CountyData/CountyData.js
+++ src/components/CountyData/CountyData.js
@@ -1,38 +1,32 @@
 import React, { Component } from "react";
-import './CountyData.scss'
-
+import "./CountyData.scss";
 
 class CountyData extends Component {
   makeTable = () => {
-       if (this.props.countyPop) {
-        return ( 
-          <div>
-        <h4>{this.props.countyName}</h4>      
+    if (this.props.countyPop) {
+      return (
+        <div>
+          <h4>{this.props.countyName}</h4>
           <table>
-          <tr>
-            <th>Positive Cases</th>
-            <th>Fatalities</th>
-            <th>County Population</th>
-          </tr>
-          <tr>
-            <td>{this.props.cases}</td>
-            <td>{this.props.deaths}</td>
-            <td>{this.props.countyPop}</td>
-          </tr>
-        </table>
+            <tr>
+              <th>Positive Cases</th>
+              <th>Fatalities</th>
+              <th>County Population</th>
+            </tr>
+            <tr>
+              <td>{this.props.cases}</td>
+              <td>{this.props.deaths}</td>
+              <td>{this.props.countyPop}</td>
+            </tr>
+          </table>
         </div>
-            )
-      }
-      else {
-          return ''
-      }
+      );
+    } else {
+      return "";
+    }
   };
   render() {
-    return (
-      <div data-testid="county-data-container">
-       {this.makeTable()}
-      </div>
-    );
+    return <div data-testid="county-data-container">{this.makeTable()}</div>;
   }
 }
 
