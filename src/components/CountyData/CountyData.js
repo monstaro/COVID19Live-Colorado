7f0dcd7c36701deab29d1df7e8b5cd8d7a6ef635
monstaro <codysmith2814@gmail.com>
2020-04-18 13:31:33 -0600

    Fixes prop structure

--- src/components/CountyData/CountyData.js
+++ src/components/CountyData/CountyData.js
@@ -3,39 +3,27 @@ import './CountyData.scss'
 
 
 class CountyData extends Component {
-  constructor({ deaths, cases, countyPop }) {
-    super({ deaths, cases, countyPop });
-    this.state = {
-      deaths: null,
-      cases: null,
-      casesSevenDaysAgo: null,
-      countyPop: null,
-    };
-  }
-  componentWillReceiveProps(nextProps) {
-    this.setState({ 
-        deaths: nextProps.deaths,
-        cases: nextProps.cases,
-        countyPop: nextProps.countyPop
-    });  
-  }
   makeTable = () => {
-      if (this.state.countyPop) {
-          return (          
-        <table>
-        <tr>
-          <th>Positive Cases</th>
-          <th>Fatalities</th>
-          <th>County Population</th>
-        </tr>
-        <tr>
-          <td>{this.state.cases}</td>
-          <td>{this.state.deaths}</td>
-          <td>{this.state.countyPop}</td>
-        </tr>
-      </table>
-          )
-      } else {
+       if (this.props.countyPop) {
+        return ( 
+          <div>
+        <h4>{this.props.countyName}</h4>      
+          <table>
+          <tr>
+            <th>Positive Cases</th>
+            <th>Fatalities</th>
+            <th>County Population</th>
+          </tr>
+          <tr>
+            <td>{this.props.cases}</td>
+            <td>{this.props.deaths}</td>
+            <td>{this.props.countyPop}</td>
+          </tr>
+        </table>
+        </div>
+            )
+      }
+      else {
           return ''
       }
   };
