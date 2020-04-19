34ed379e5d8ec9085d9541596f1fefc27a756463
monstaro <codysmith2814@gmail.com>
2020-04-19 16:52:57 -0600

    Adds unit test

--- src/containers/LocalHealth/LocalHealth.test.js
+++ src/containers/LocalHealth/LocalHealth.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
