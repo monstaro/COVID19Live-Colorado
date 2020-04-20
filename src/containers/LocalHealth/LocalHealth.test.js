06fe3692761151f856c5cd5b8b9e807c63cbd150
monstaro <codysmith2814@gmail.com>
2020-04-20 17:11:00 -0600

    Adds test for rendering and state

--- src/containers/LocalHealth/LocalHealth.test.js
+++ src/containers/LocalHealth/LocalHealth.test.js
@@ -36,7 +36,17 @@ describe("Local Health", () => {
 
   })
   it("should render", () => {
-    const store = createStore(rootReducer);
+    const initState = { healthDepts: [{
+      "id": "1",
+      "name": "Adams County",
+      "website": "http://www.tchd.org/",
+      "twitter": "https://twitter.com/TCHDHealth",
+      "facebook": "https://www.facebook.com/cotchd/",
+      "rss": "",
+      "phone": "(303) 220-9200"
+    }]
+  }
+    const store = createStore(rootReducer, initState);
     const { getByTestId } = render(
       <Provider store={store}>
         <Router>
@@ -45,6 +55,41 @@ describe("Local Health", () => {
       </Provider>
     );
     expect(getByTestId("local-health-container")).toBeInTheDocument();
-  });
+    expect(getByTestId("Adams County")).toBeInTheDocument();
 
+  });
+  it("should contain dept info if it has state", () => {
+    const initState = { healthDepts: [{
+      "id": "1",
+      "name": "Adams County",
+      "website": "http://www.tchd.org/",
+      "twitter": "https://twitter.com/TCHDHealth",
+      "facebook": "https://www.facebook.com/cotchd/",
+      "rss": "",
+      "phone": "(303) 220-9200"
+    }]
+  }
+    const store = createStore(rootReducer, initState);
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <Router>
+          <LocalHealth />
+        </Router>
+      </Provider>
+    );
+    expect(getByTestId('dept-info')).toBeInTheDocument()
+  })
+  // it("should not contain dept info if it has no state", () => {
+  //   const initState = { healthDepts: []
+  // }
+  //   const store = createStore(rootReducer, initState);
+  //   const { getByTestId } = render(
+  //     <Provider store={store}>
+  //       <Router>
+  //         <LocalHealth />
+  //       </Router>
+  //     </Provider>
+  //   );
+  //   expect(getByTestId('dept-info')).toBeNull()
+  // })
 });
