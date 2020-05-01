bce14604eab046930fe5c7c1d96dd66cf9391ca8
monstaro <codysmith2814@gmail.com>
2020-05-01 12:57:00 -0600

    Fixes basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import rootReducer from "./reducers";
 
 const store = createStore(rootReducer, composeWithDevTools());
 const router = (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename="/welcome">
     <App></App>
   </BrowserRouter>
 );
